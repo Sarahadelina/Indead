88a6b6c90c04632f0a2fa5f267bddaf0694503c7
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-07-26 17:50:11 +0200

    Projet remis au propre suite à des incidents

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useBasket } from "../hooks/useContext";
+import logo from "../assets/Images/logo-site.png";
+import "../styles/navbar.css";
+
+
+export default function Navbar() {
+  const { basket } = useBasket();
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  }
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo-container">
+          <img src={logo} alt="logo indead" />
+        </div>
+        <div className="navbar_basket">
+          <button onClick={togglePopup}>
+            {basket.length
+              ? `Il y a ${basket.length} articles dans votre panier`
+              : "Votre panier est vide"}
+          </button>
+        </div>
+      </nav>
+
+      {isPopupVisible && (
+        <div className="popup">
+          <div className="popup_inner">
+            <h2>Votre Panier</h2>
+            <button className="close" onClick={togglePopup}>X</button>
+            {basket.length > 0 ? (
+              <ul className="basket_items">
+                {basket.map((item, index) => (
+                  <li key={index} className="basket_item">
+                    <div className="item_details">
+                      <p className="item_name">{item.nom}</p>
+                      <p className="item_price">{item.prix} {item.devise}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Votre panier est vide</p>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { getListProduct } from './services/request.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BasketProvider } from './context/BasketContext';
+import App from './App.jsx';
+import Home from './pages/Home';
+import './styles/home.css';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: getListProduct,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BasketProvider>
+      <RouterProvider router={router} />
+    </BasketProvider>
   </React.StrictMode>,
-)
+);
